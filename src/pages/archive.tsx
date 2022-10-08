13e0b5737cc02e322b5c5b6c5c1e1f7371b66930
Anand Bose <anandbose6@icloud.com>
2022-10-08 13:58:49 +0530

    Implement archives page

---
+++ src/pages/archive.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react'
+import { HeadFC } from 'gatsby'
+import Layout from '../components/Layout'
+import { Contents, Content } from '../Contents'
+import { Months } from '../util/FormatDate'
+import { Link } from 'gatsby'
+
+const ArchivePage = () => {
+    const groupedContents = new Map<string, Array<Content>>()
+
+    Contents.forEach(content => {
+        const date = new Date(content.date)
+        const monthAndYear = `${Months[date.getMonth()]} ${date.getFullYear()}`
+        const array = groupedContents.has(monthAndYear) ? groupedContents.get(monthAndYear)! : new Array<Content>()
+        array.push(content)
+        groupedContents.set(monthAndYear, array)
+    })
+
+    return (
+        <Layout>
+            <main>
+                <h1 id="archive">Archive</h1>
+                <p>Browse all posts by month and year.</p>
+                { Array.from(groupedContents.keys()).map(monthAndYear => (
+                    <>
+                        <h2>{monthAndYear}</h2>
+                        <ul>
+                            { groupedContents.get(monthAndYear)?.map(content => (
+                                <li><Link to={content.path}>{content.title}</Link></li>
+                            )) }
+                        </ul>
+                    </>
+                )) }
+            </main>
+        </Layout>
+    )
+}
+
+export default ArchivePage
+
+export const Head: HeadFC = () => <title>Archive</title>
--- src/util/FormatDate.ts
+++ src/util/FormatDate.ts
@@ -1,10 +1,10 @@
 
-const months: Array<string> = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-const days: Array<string> = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+export const Months: Array<string> = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+export const Days: Array<string> = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 
 export function FormatDate(input: string): string {
     const parsedDate = new Date(input)
-    const day = days[parsedDate.getDay()]
-    const month = months[parsedDate.getMonth()]
+    const day = Days[parsedDate.getDay()]
+    const month = Months[parsedDate.getMonth()]
     return `${day}, ${parsedDate.getDate()} ${month} ${parsedDate.getFullYear()}`
 }
