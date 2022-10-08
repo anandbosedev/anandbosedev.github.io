35bd9e45ea9699a1616e7126e91bfe609d62aae2
Anand Bose <anandbose6@icloud.com>
2022-10-08 14:23:55 +0530

    Implement categories page

---
+++ src/pages/categories.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react'
+import Layout from '../components/Layout'
+import { HeadFC } from 'gatsby'
+import { Content, Contents} from '../Contents'
+import { Link } from 'gatsby'
+
+const Categories = () => {
+    const groupedContents = new Map<string, Array<Content>>()
+    Contents.forEach(content => {
+        const array = groupedContents.has(content.category) ? groupedContents.get(content.category)! : new Array<Content>()
+        array.push(content)
+        groupedContents.set(content.category, array)
+    })
+    return (
+        <Layout>
+            <main>
+                <h1 id="categories">Categories</h1>
+                <p>Browse all posts by categories.</p>
+                { Array.from(groupedContents.keys()).map(category => (
+                    <>
+                        <h2>{category}</h2>
+                        <ul>
+                            { groupedContents.get(category)?.map(content => (
+                                <li><Link to={content.path}>{content.title}</Link></li>
+                            ))}
+                        </ul>
+                    </>
+                ))}
+            </main>
+        </Layout>
+    )
+}
+
+export default Categories
+export const Head: HeadFC = () => <title>Categories</title>
