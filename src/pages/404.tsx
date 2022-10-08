3cddd338e926102ef28743a0c7a4f646ae0cb870
Anand Bose <anandbose6@icloud.com>
2022-10-08 16:07:34 +0530

    Update 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -9,7 +9,7 @@ const NotFoundPage = () => {
         <h1>404 Not Found</h1>
         <p>Sorry, the page you requested not found in the server.</p>
         <aside className="related">
-          <p>May be the link you typed has mistakes, or permanently removed from the server.</p>
+          <p>I am migrating the blog from Jekyll to Gatsby, and rewriting them entirely in React. Some pages are <i>still</i> left to migrate. Please stay tuned.</p>
         </aside>
       </main>
     </Layout>
