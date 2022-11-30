c0447d39fa62d21f8c0272b9294547294c720595
Anand Bose <anandbose6@icloud.com>
2022-11-30 23:24:09 +0530

    Update footer to include Mastodon profile link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,7 +10,7 @@ const Footer = () => (
         <StaticImage alt="Anand Bose" width={100} height={100} src='../images/avatar.jpg' imgClassName="avatar" />
         <small style={FooterFirstLineStyle}>Hi there! I am Anand Bose.</small>
         <small>👨🏻‍💻📲</small>
-        <small>Feel free to follow me on <a href="https://twitter.com/anandbosedev">Twitter</a> or <a href="https://github.com/anandbosedev">GitHub</a>.</small>
+        <small>Feel free to follow me on <a href="https://twitter.com/anandbosedev">Twitter</a> / <a rel="me" href="https://mastodon.social/@anandbose">Mastodon</a> / <a href="https://github.com/anandbosedev">GitHub</a>.</small>
         <small>
           Anand Bose © <time dateTime="2022-10-03T13:58:50+00:00">2022</time>. All rights reserved.
         </small>
